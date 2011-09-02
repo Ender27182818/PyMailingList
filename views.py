0a97fd6313273d9a4a371d644c0a7de9825de7f5
Eli Ribble <junk@theribbles.org>
2011-09-02 11:41:09 -0600

    Added my first view

--- views.py
+++ views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render_to_response
 from PyMailingList.models import Message
 
 def index(request):
-	return render_to_response('PyMailingList/index.html', {})
+	messages = Message.objects.all()
+	return render_to_response('PyMailingList/index.html', {'messages':messages})
