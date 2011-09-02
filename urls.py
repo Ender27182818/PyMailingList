b2468a984764cf513abfe10123565b99e1293533
Eli Ribble <junk@theribbles.org>
2011-09-02 11:52:36 -0600

    Added a view for showing the whole email and a link to the email in question

--- urls.py
+++ urls.py
@@ -3,7 +3,9 @@ from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-	(r'^$', 'PyMailingList.views.index'),
+urlpatterns = patterns('PyMailingList.views',
+	(r'^$', 'index'),
+	(r'^/$', 'index'),
+	(r'^message/(?P<message_id>\d+)/$', 'show_message'),
 	url(r'^admin/', include(admin.site.urls)),
 )
--- views.py
+++ views.py
@@ -5,3 +5,7 @@ from PyMailingList.models import Message
 def index(request):
 	messages = Message.objects.all()
 	return render_to_response('PyMailingList/index.html', {'messages':messages})
+
+def show_message(request, message_id):
+	message = Message.objects.get(pk=message_id)
+	return render_to_response('PyMailingList/show_message.html', {'message':message})
