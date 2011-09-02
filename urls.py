f9d63ecfa11096d3e61d9f6f65a9681e9180057f
Eli Ribble <junk@theribbles.org>
2011-09-02 13:53:34 -0600

    Fixed a repeated url config which was breaking the host site

--- urls.py
+++ urls.py
@@ -5,7 +5,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('PyMailingList.views',
 	(r'^$', 'index'),
-	(r'^/$', 'index'),
 	(r'^message/(?P<message_id>\d+)/$', 'show_message'),
 	url(r'^admin/', include(admin.site.urls)),
 )
